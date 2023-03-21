20c390eb8645d9e48fa0988d029b23379c84560e
chandrakant sir <cakant70@gmail.com>
2023-03-22 00:46:56 +0530

    day 1 spring with jpa, hibernate and lombok

---
+++ src/main/java/com/example/demo/SpringHiberJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHiberJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHiberJpaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/model/Employee.java
@@ -0,0 +1,28 @@
+package com.example.demo.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "emptbl")
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	@Column(name = "first_name", nullable = false)
+	private String firstName;
+	@Column(name = "last_name", nullable = false)
+	private String lastName;
+	@Column(name = "email", nullable = false)
+	private String email;
+
+	
+	
+}
